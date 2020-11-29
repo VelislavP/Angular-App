4a32c5131ae84464386cf01bec8b5bc27af58cbf
Velislav <velislav7777777@gmail.com>
2020-11-29 17:59:50 +0200

    message

---
+++ src/app/_helpers/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthenticationService } from '../_services';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser){
+      //if logged in return true
+      return true;
+    }
+    this.router.navigate(['/login'],{queryParams: {returnURL: state.url}});
+    return false;
+  }
+  
+}
